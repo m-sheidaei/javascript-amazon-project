913d032e3cb634016fe46cba84100eeadd9fe852
m-sheidaei <msheidaei1@gmail.com>
2025-05-03 09:57:57 +0330

    move addToCart into cart.js

--- data/carts.js
+++ data/carts.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId, parent) {
+  let machingItem;
+  const quantity = parseInt(parent.querySelector(".quantity-select").value);
+
+  cart.map((item) => {
+    if (productId === item.id) {
+      machingItem = item;
+    }
+  });
+
+  if (machingItem) {
+    machingItem.quantity += quantity;
+  } else {
+    cart.push({
+      id: productId,
+      quantity: quantity,
+    });
+  }
+}
